7e4bc08f8007be4b07a42441cdebf87bb6a02996
LucasSaMo <lucassalgado075@gmail.com>
2025-08-12 15:00:27 -0300

    primeiro commit

--- src/api.js
+++ src/api.js
@@ -1,11 +1,11 @@
 export const geoApiOptions = {
   method: "GET",
   headers: {
-    "X-RapidAPI-Key": "",// enter your rapid api key here
-    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+    'x-rapidapi-key': 'b0e1e3f59fmsh8a09d5fdeb6deb3p1e7365jsn02dab1e891c0',
+    'x-rapidapi-host': 'wft-geo-db.p.rapidapi.com'
   },
 };
 export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 
 export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
-export const WEATHER_API_KEY = ""; // enter your key from openweather API
+export const WEATHER_API_KEY = "247c34849c5d5c6a3e1ed2106bcebd5c";
